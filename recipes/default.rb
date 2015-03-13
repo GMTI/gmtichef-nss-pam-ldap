29c33f8af7a8d327b0b54c23706d75d8cae265e3
Dann S Washko <dwashko@gmti.gannett.com>
2015-03-13 15:59:44 -0400

    GDPINF-106 - converted specific attributes to data bag attributes.

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,10 +4,13 @@
 
 package node['nslcd']['package']
 
+nssbagconfigs = data_bag_item(node['nslcd']['data_bag'], node['nslcd']['data_bag_name'])
+
 template '/etc/nslcd.conf' do
   source 'nslcd.conf.erb'
   mode '0600'
   variables(
+    nssbagconfigs: nssbagconfigs,
     settings:  node['nslcd']
     )
 end
